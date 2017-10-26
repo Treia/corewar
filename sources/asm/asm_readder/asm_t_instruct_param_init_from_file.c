189cc22121d386dd7153372f3974d9956455ca8d
Mathilde RESSIER <ressier@stg-interactive.com>
2017-10-26 15:45:10 +0200

    toto

--- sources/asm/asm_readder/asm_t_instruct_param_init_from_file.c
+++ sources/asm/asm_readder/asm_t_instruct_param_init_from_file.c
@@ -46,7 +46,7 @@ int				asm_t_instruct_param_init_from_file(t_parser *parser,
 		return (asm_message_error(SYNTAX_ERR, parser->current_ptr, ptr));
 	one_param = internal_dup_and_trim(parser->current_ptr, ptr);
 	if (asm_t_instruct_param_add_end(instruct->param,
-		one_param) == EXIT_FAILURE)
+			one_param) == EXIT_FAILURE)
 	{
 		ft_memdel((void **)&one_param);
 		return (asm_param_error(instruct, parser->file_content,
